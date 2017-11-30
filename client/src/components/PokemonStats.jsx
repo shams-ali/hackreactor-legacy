c3fbedc197e34faf063ca5ec7be59ad6491348c7
v1x1e <github@victoriahchang.com>
2017-11-29 19:36:18 -0800

    Add and style HP bar

--- client/src/components/PokemonStats.jsx
+++ client/src/components/PokemonStats.jsx
@@ -1,14 +1,26 @@
 import React, { Component } from 'react';
 import css from '../styles.css';
 
+
 const PokemonStats = (props) => {
+  let barWidth = '50px';
+  let hpWidth = '50px';
+
   return (
     <div className={css.stats}>
       <h2>{props.stats.name.toUpperCase()}</h2>
+      <div
+        // style={}
+        className={css.hpBar}>
+        <div
+          className={css.hp}
+          style={{width: width}}>
+        </div>
+      </div>
       <h4 style={{marginBottom: '2px'}}> {props.stats.health} / {props.stats.initialHealth} </h4>
       <h6 style={{marginTop: '5px'}}> atk: {props.stats.attack} - def: {props.stats.defense} </h6>
     </div>
   )
 }
 
-export default PokemonStats; 
+export default PokemonStats;
