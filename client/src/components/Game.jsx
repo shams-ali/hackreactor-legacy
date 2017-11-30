aa0d794e64254c693f752bed9046e38c8251a4f2
Daniel Roberts <BeardOfDan@gmail.com>
2017-11-29 17:15:44 -0800

    (refactor) Fix syntax

--- client/src/components/Game.jsx
+++ client/src/components/Game.jsx
@@ -11,7 +11,7 @@ import GameState from './GameState.jsx';
 import Logo from './Logo.jsx';
 import css from '../styles.css';
 
-import help from './../../../utils/helpers.js'
+import help from './../../../utils/helpers.js';
 
 export default class Game extends Component {
   constructor(props) {
@@ -30,9 +30,9 @@ export default class Game extends Component {
       winner: null,
       chatInput: '',
       commandInput: '',
-      commandArray: [{command: `The game will begin shortly - type 'help' to learn how to play`}],
+      commandArray: [{ command: 'The game will begin shortly - type \'help\' to learn how to play' }],
       socket: null
-    }
+    };
 
     this.handleInputChange = this.handleInputChange.bind(this);
     this.handleChatInputSubmit = this.handleChatInputSubmit.bind(this);
@@ -42,15 +42,15 @@ export default class Game extends Component {
   socketHandlers() {
     return {
       handleChat: (message) => {
-      var messageInstance = {
+        var messageInstance = {
           name: message.name,
           text: message.text
-        }
+        };
         this.setState(prevState => {
           return {
             messageArray: prevState.messageArray.concat(messageInstance)
-          }
-        })
+          };
+        });
       },
       playerInitialized: (data) => {
         this.setState({
@@ -71,14 +71,14 @@ export default class Game extends Component {
           });
         }
         this.setState({
-          commandArray: [{command: 'Let the battle begin!'}]
+          commandArray: [{ command: 'Let the battle begin!' }]
         });
       },
       attackProcess: (data) => {
         this.setState(prevState => {
           return {
             commandArray: prevState.commandArray.concat(data.basicAttackDialog)
-          }
+          };
         });
       },
       turnMove: (data) => {
@@ -88,7 +88,7 @@ export default class Game extends Component {
               pokemon: data.player1.pokemon,
               opponent: data.player2,
               isActive: !prevState.isActive
-            }
+            };
           });
         } else {
           this.setState(prevState => {
@@ -96,8 +96,8 @@ export default class Game extends Component {
               pokemon: data.player2.pokemon,
               opponent: data.player1,
               isActive: !prevState.isActive
-            }
-          })
+            };
+          });
         }
       },
       gameOver: (data) => {
@@ -115,8 +115,8 @@ export default class Game extends Component {
           winner: data.name,
           gameOver: true,
           isActive: false
-        }); 
-        setTimeout(() => this.props.history.replace("/"), 20000); 
+        });
+        setTimeout(() => this.props.history.replace('/'), 20000);
       },
       seppuku: (data) => {
         console.log('in socketHandlers - seppuku', data);
@@ -127,10 +127,10 @@ export default class Game extends Component {
           winner: this.state.opponent.name,
           gameOver: true,
           isActive: false
-        }); 
-        setTimeout(() => this.props.history.replace("/"), 20000); 
+        });
+        setTimeout(() => this.props.history.replace('/'), 20000);
       }
-    }
+    };
   }
 
   componentDidMount() {
@@ -142,26 +142,25 @@ export default class Game extends Component {
           this.setState({
             name: username,
             socket
-          })
+          });
           const playerInit = {
             gameid: this.props.match.params.gameid,
             name: username,
             pokemon: this.state.pokemon
-          }
+          };
           socket.emit('join game', playerInit);
-          socket.on('gamefull', message => alert(message)); 
-          socket.on('chat message', this.socketHandlers().handleChat); 
-          socket.on('player', this.socketHandlers().playerInitialized); 
-          socket.on('ready', this.socketHandlers().handleReady); 
-          socket.on('attack processed', this.socketHandlers().attackProcess); 
+          socket.on('gamefull', message => alert(message));
+          socket.on('chat message', this.socketHandlers().handleChat);
+          socket.on('player', this.socketHandlers().playerInitialized);
+          socket.on('ready', this.socketHandlers().handleReady);
+          socket.on('attack processed', this.socketHandlers().attackProcess);
           socket.on('turn move', this.socketHandlers().turnMove);
-          socket.on('gameover', this.socketHandlers().gameOver); 
-          socket.on('seppuku', this.socketHandlers().seppuku); 
+          socket.on('gameover', this.socketHandlers().gameOver);
+          socket.on('seppuku', this.socketHandlers().seppuku);
+        } else {
+          this.props.history.replace('/login');
         }
-        else {
-          this.props.history.replace("/login");
-        }
-      })
+      });
   }
 
   handleInputChange(e, type) {
@@ -177,8 +176,8 @@ export default class Game extends Component {
     if (e.keyCode === 13) {
       var socket = io();
       this.state.socket.emit('chat message', {
-        gameid: this.props.match.params.gameid, 
-        name: this.state.name, 
+        gameid: this.props.match.params.gameid,
+        name: this.state.name,
         text: e.target.value
       });
       this.setState({
@@ -194,8 +193,8 @@ export default class Game extends Component {
           return {
             commandArray: prevState.commandArray.concat(help),
             commandInput: ''
-          }
-        })
+          };
+        });
       },
       attack: () => {
         this.state.socket.emit('attack', {
@@ -204,7 +203,7 @@ export default class Game extends Component {
           pokemon: this.state.pokemon
         });
         this.setState({
-            attacking: false
+          attacking: false
         });
       },
       choose: (pokemonToSwap) => {
@@ -215,7 +214,7 @@ export default class Game extends Component {
           if (poke.name === pokemonToSwap) {
             isAvailable = true;
             index = i;
-            health = poke.health 
+            health = poke.health;
           }
         });
         if (isAvailable && health > 0) {
@@ -223,9 +222,9 @@ export default class Game extends Component {
             gameid: this.props.match.params.gameid,
             pokemon: this.state.pokemon,
             index
-          })
+          });
         } else if (health === 0) {
-          alert('That pokemon has fainted!')
+          alert('That pokemon has fainted!');
         } else {
           alert('You do not have that pokemon!');
         }
@@ -238,23 +237,26 @@ export default class Game extends Component {
           name: opponentName
         });
       }
-    }
+    };
   }
 
 
 
   handleCommands(e) {
-    if (e.keyCode !== 13) return;
-    let value = e.target.value.toLowerCase(); 
+    let value = e.target.value.toLowerCase();
+
+    if (e.keyCode !== 13) {
+      return undefined;
+    }
 
     if (value === 'help') {
-      return this.commandHandlers().help(); 
+      return this.commandHandlers().help();
     }
 
     if (value === 'seppuku') {
       return this.commandHandlers().seppuku();
     }
-    
+
     if (!this.state.isActive) {
       alert('it is not your turn!');
     } else {
@@ -264,20 +266,20 @@ export default class Game extends Component {
         } else {
           this.setState({
             attacking: true
-          })
-          setTimeout(() => this.commandHandlers().attack(), 300);  
+          });
+          setTimeout(() => this.commandHandlers().attack(), 300);
         }
-      } else if (value.split(' ')[0] === "choose") {
-        this.commandHandlers().choose(value.split(' ')[1]); 
+      } else if (value.split(' ')[0] === 'choose') {
+        this.commandHandlers().choose(value.split(' ')[1]);
       } else {
-        alert('invalid input!')
+        alert('invalid input!');
       }
     }
 
     this.setState({
       commandInput: ''
     });
-  
+
   }
 
   renderGame() {
@@ -287,11 +289,11 @@ export default class Game extends Component {
         <div className={css.loading}>
           <h1>Awaiting opponent...</h1>
         </div>
-      )
+      );
     } else if (this.state.gameOver) {
-      return <GameOverView pokemon={winner === name ? pokemon : opponent.pokemon} winner={winner} />
+      return <GameOverView pokemon={winner === name ? pokemon : opponent.pokemon} winner={winner} />;
     } else {
-      return <GameView opponent={opponent} pokemon={pokemon} attacking={attacking} />
+      return <GameView opponent={opponent} pokemon={pokemon} attacking={attacking} />;
     }
   }
 
@@ -300,7 +302,7 @@ export default class Game extends Component {
       <div className={css.stateContainer}>
         <Logo name={this.state.name} isActive={this.state.isActive} opponent={this.state.opponent} />
         <GameState pokemon={this.state.pokemon} />
-        <Chat messageArray={this.state.messageArray} chatInput={this.state.chatInput} handleChatInputSubmit={this.handleChatInputSubmit} handleInputChange={this.handleInputChange} /> 
+        <Chat messageArray={this.state.messageArray} chatInput={this.state.chatInput} handleChatInputSubmit={this.handleChatInputSubmit} handleInputChange={this.handleInputChange} />
       </div>
     );
   }
@@ -316,7 +318,7 @@ export default class Game extends Component {
         </div>
         {this.renderSideBar()}
       </div>
-    )
+    );
   }
 }
 
