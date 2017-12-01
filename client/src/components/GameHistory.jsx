25f20e49a1205e9c76a8c4d0057e0e183e7fa33d
philcho <Phil Cho>
2017-12-01 14:38:10 -0800

    Fetch last 10 games from DB and display:

--- client/src/components/GameHistory.jsx
+++ client/src/components/GameHistory.jsx
@@ -2,14 +2,13 @@ import React, { Component } from 'react';
 import GameHistoryRow from './GameHistoryRow.jsx';
 import css from '../styles.css';
 import hist from '../gameHistory.css';
-import gameHistoryData from '../../../test/exampleGameHistoryData.js';
 
 const GameHistory = (props) => {
   return (
     <div className={hist.container}>
       <h1 className={hist.header}>Win/Loss Record</h1>
       <div className={hist.gamesList}>
-        {gameHistoryData.games.map(game => {
+        {props.gameHistoryData.map(game => {
           return (
             <GameHistoryRow game={game} key={game.id} />
           );
