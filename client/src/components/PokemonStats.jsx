6999a0d93e67b8de7e2f50b8147fe28bd742ddd5
v1x1e <github@victoriahchang.com>
2017-12-03 20:52:32 -0800

    (fix) Center Pokemon and HP bars on screen, fixes #85

--- client/src/components/PokemonStats.jsx
+++ client/src/components/PokemonStats.jsx
@@ -1,10 +1,6 @@
 import React, { Component } from 'react';
 import css from '../PokemonStats.css';
 
-const pokemonTypesAnimations = {
-
-};
-
 const PokemonStats = (props) => {
   const damage = props.stats.health / props.stats.initialHealth;
 
